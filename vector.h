3c8ec6114c5452a5544f936381483e117ec5de18
Lucien Reboul <lucien.reboul@grenoble-inp.org>
2024-09-26 14:40:14 +0200

    Implement epc::vector All test run without error locally

--- vector.h
+++ vector.h
@@ -9,28 +9,126 @@ namespace epc
     class vector
     {
     public:
-        vector() noexcept { }        
+        static const int GROW_FACTOR = 2;
+        size_t capacity_;
+        size_t size_;
+        T* data_;
 
-        vector(const vector&) { }
-        vector& operator=(const vector&) { }
+        vector() noexcept {
+            capacity_ = 0;
+            size_ = 0;
+            data_ = nullptr;
+         }        
 
-        ~vector() { }
+        vector(const vector& other) 
+            :capacity_(other.capacity_), size_(other.size_)
+        {
+            size_ == 0 ? data_ = nullptr : data_ = new T[capacity_];
+            try {
+                std::copy(other.data_, other.data_ + size_, data_);
+            }
+            catch(...) {
+                delete[] data_;
+                throw;
+            }
+        }
+        
+        vector& operator=(const vector& other) { 
+            if(this == &other ) {
+                return *this;
+            }
 
-        T* data() { }
-        const T* data() const { }
+            T* new_data;
+            other.size_ == 0 ? new_data = nullptr : new_data = new T[other.capacity_];
 
-        T& operator[](size_t) { }
-        const T& operator[](size_t) const { }
+            try {
+                std::copy(other.data_, other.data_ + other.size_, new_data);
+            }
+            catch(...) {
+                delete[] new_data;
+                throw;
+            }
 
-        void push_back(const T&) { }
-        void pop_back() { }
+            delete[] data_; 
 
-        void reserve(size_t) { }
+            data_ = new_data;
+            capacity_ = other.capacity_;
+            size_ = other.size_;
 
-        size_t capacity() const { }
-        size_t size() const { }
+            return *this;
+        }
 
-        void swap(vector&) noexcept { }
+        ~vector() {
+            delete[] data_;
+         }
+
+        T* data() { return data_; }
+        const T* data() const { return data_;} 
+
+        T& operator[](size_t index) { 
+            return data_[index];
+         }
+        const T& operator[](size_t index) const {
+            return data_[index];
+        }
+
+        void push_back(const T& value) { 
+            if(size_ < capacity_){
+                data_[size_] = value;
+                size_++;
+            }
+            else {
+                if (capacity_ == 0) {
+                    this->reserve(1);
+                }
+                else {
+                    this->reserve(capacity_ * GROW_FACTOR);
+                }
+                this->push_back(value);
+            }
+        }
+        void pop_back() { 
+            size_--;
+        }
+
+        void reserve(size_t capacity) { 
+            if (capacity <= capacity_) {
+                return;
+            }
+        
+            T* new_data = new T[capacity];
+
+            try {
+                std::copy(data_, data_ + size_, new_data);
+            }
+            catch(...) {
+                delete[] new_data;
+                throw;
+            }
+
+            delete[] data_;
+           
+            data_ = new_data;
+            capacity_ = capacity;            
+        }
+
+        size_t capacity() const { return capacity_; }
+        size_t size() const { return size_; }
+
+        void swap(vector& other) noexcept {
+
+            size_t capacity_tmp = capacity_;
+            size_t size_tmp = size_;
+            T* data_tmp = data_;
+
+            capacity_ = other.capacity_;
+            size_ = other.size_;
+            data_=other.data_;
+
+            other.capacity_ = capacity_tmp;
+            other.size_ = size_tmp;
+            other.data_ = data_tmp;
+        }
     };
 }
 
